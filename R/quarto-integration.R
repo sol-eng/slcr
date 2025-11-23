819db42a2046d27bd4518da0a9adef2d91df224b
Michael Mayer <michael.mayer@posit.co>
2025-11-23 20:06:56 +0000

    better slc code chunk integration

---
+++ R/quarto-integration.R
@@ -0,0 +1,46 @@
+#' Get path to SLC Quarto resources
+#' @param file The resource file name
+#' @return Path to the resource file
+#' @export
+slc_resource_path <- function(file) {
+  system.file("resources", file, package = "slcr")
+}
+
+#' Include SLC Quarto resources in document
+#' @return HTML content for SLC styling and collapsible functionality
+#' @export
+slc_quarto_resources <- function() {
+  html_path <- system.file("resources", "slc-quarto.html", package = "slcr")
+  
+  if (!file.exists(html_path)) {
+    warning("SLC Quarto resources not found. Make sure slcr package is properly installed.")
+    return("")
+  }
+  
+  # Read the HTML content
+  html_content <- paste(readLines(html_path, warn = FALSE), collapse = "\n")
+  
+  # Return raw HTML that will be included directly
+  return(htmltools::HTML(html_content))
+}
+
+#' Setup SLC for Quarto (called automatically when library is loaded)
+#' @export
+setup_slc_quarto <- function() {
+  # Register the knitr engine if not already registered
+  if (is.null(knitr::knit_engines$get("slc"))) {
+    knitr::knit_engines$set(slc = slc_engine)
+  }
+
+  # Auto-include resources if in a Quarto context
+  if (is_quarto_context()) {
+    cat(as.character(slc_quarto_resources()))
+  }
+}
+
+#' Check if we're in a Quarto rendering context
+#' @return Logical indicating if in Quarto context
+is_quarto_context <- function() {
+  !is.null(knitr::opts_knit$get("rmarkdown.pandoc.to")) ||
+    Sys.getenv("QUARTO_PROJECT_DIR") != ""
+}
--- R/zzz.R
+++ R/zzz.R
@@ -17,3 +17,15 @@
   # Register the SLC engine with knitr
   knitr::knit_engines$set(slc = slc_engine)
 }
+
+.onLoad <- function(libname, pkgname) {
+  # Register SLC engine
+  knitr::knit_engines$set(slc = slc_engine)
+}
+
+.onAttach <- function(libname, pkgname) {
+  # Setup Quarto integration when package is attached
+  if (interactive() && is_quarto_context()) {
+    setup_slc_quarto()
+  }
+}

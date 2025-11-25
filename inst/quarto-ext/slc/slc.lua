49f96461fe7937e1a7661fe334e5919339ddfa2c
Michael Mayer <michael.mayer@posit.co>
2025-11-25 15:34:32 +0000

    fix quarto directory issue

--- inst/quarto-ext/slc/slc.lua
+++ inst/quarto-ext/slc/slc.lua
@@ -1,6 +1,9 @@
 function Meta(meta)
   -- Read the shared HTML resource file
-  local handle = io.open(quarto.project.directory .. "/_extensions/slc/slc-resources.html", "r")
+  local directory = quarto.project and quarto.project.directory or "."
+  local resource_path = directory .. "/_extensions/slc/slc-resources.html"
+
+  local handle = io.open(resource_path, "r")
   if not handle then
     -- Fallback: try to find it in the R package installation
     -- This is a simplified approach - in practice you'd need to locate the R package

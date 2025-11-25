fa34c6552ba438854b84c2f7e39f09a41b600203
Michael Mayer <michael.mayer@posit.co>
2025-11-25 13:21:30 +0000

    add quarto extension

---
+++ inst/quarto-ext/slc/slc.lua
@@ -0,0 +1,19 @@
+function Meta(meta)
+  -- Read the shared HTML resource file
+  local handle = io.open(quarto.project.directory .. "/_extensions/slc/slc-resources.html", "r")
+  if not handle then
+    -- Fallback: try to find it in the R package installation
+    -- This is a simplified approach - in practice you'd need to locate the R package
+    return meta
+  end
+
+  local resources = handle:read("*a")
+  handle:close()
+
+  quarto.doc.include_text("in-header", resources)
+  return meta
+end
+
+return {
+  { Meta = Meta }
+}
